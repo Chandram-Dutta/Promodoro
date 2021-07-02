2027f026802d9d9dfb7f27717dda1bef756442a8
Chandram-Dutta <chandramdutta2004@gmail.com>
2021-07-02 11:35:16 +0530

    Minor Changes

--- lib/main.dart
+++ lib/main.dart
@@ -31,15 +31,32 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  int rounds = 0;
+
   double totalPercent = 0;
-  double totaltimeMin = 0;
-  double totaltimeSec = 0;
+  double totalTimeMin = 0;
+  double totalTimeSec = 0;
+  double totalTimePassed = 0;
+  bool totalIsRunning = false;
+
   double restPercent = 0;
-  double resttimeMin = 0;
-  double resttimeSec = 0;
+  double restTimeMin = 0;
+  double restTimeSec = 0;
+  double restTimePassed = 0;
+  bool restIsRunning = false;
+
   double workPercent = 0;
-  double worktimeMin = 0;
-  double worktimeSec = 0;
+  double workTimeMin = 0;
+  double workTimeSec = 0;
+  double workTimePassed = 0;
+  bool workIsRunning = false;
+
+  @override
+  void initState() {
+    super.initState();
+    totalTimeSec = totalTimeMin * 60;
+    totalPercent = (totalTimePassed / totalTimeMin) * 100;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -58,10 +75,6 @@ class _HomeState extends State<Home> {
       body: Padding(
         padding: const EdgeInsets.all(10.0),
         child: Container(
-          decoration: BoxDecoration(
-              border:
-                  Border.all(color: Theme.of(context).accentColor, width: 5),
-              borderRadius: BorderRadius.circular(10)),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
@@ -69,100 +82,119 @@ class _HomeState extends State<Home> {
               Container(
                   child: Column(
                 children: [
-                  Center(child: Text("Pomodoro Indicator")),
-                  SizedBox(height: 30),
-                  CircularPercentIndicator(
-                    backgroundColor: Colors.transparent,
-                    reverse: true,
-                    circularStrokeCap: CircularStrokeCap.round,
-                    radius: 220.0,
-                    lineWidth: 15.0,
-                    percent: 0.9,
-                    animation: true,
-                    animateFromLastPercent: true,
-                    progressColor: Colors.lightGreenAccent[400],
-                    center: CircularPercentIndicator(
-                      backgroundColor: Colors.transparent,
-                      reverse: true,
-                      circularStrokeCap: CircularStrokeCap.round,
-                      radius: 180.0,
-                      lineWidth: 15.0,
-                      percent: 0.4,
-                      animation: true,
-                      progressColor: Colors.pinkAccent[400],
-                      center: CircularPercentIndicator(
-                        backgroundColor: Colors.transparent,
-                        reverse: true,
-                        circularStrokeCap: CircularStrokeCap.round,
-                        radius: 140.0,
-                        lineWidth: 15.0,
-                        percent: 0.5,
-                        animation: true,
-                        progressColor: Colors.lightBlueAccent[700],
-                        center: IconButton(
-                          onPressed: () {},
-                          icon: Icon(CupertinoIcons.playpause_fill),
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                            color: Theme.of(context).accentColor, width: 5),
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Column(
+                      children: [
+                        Center(child: Text("Pomodoro Indicator")),
+                        SizedBox(height: 30),
+                        CircularPercentIndicator(
+                          backgroundColor: Colors.transparent,
+                          reverse: true,
+                          circularStrokeCap: CircularStrokeCap.round,
+                          radius: 220.0,
+                          lineWidth: 15.0,
+                          percent: totalPercent,
+                          animation: true,
+                          animateFromLastPercent: true,
+                          progressColor: Colors.lightGreenAccent[400],
+                          center: CircularPercentIndicator(
+                            backgroundColor: Colors.transparent,
+                            circularStrokeCap: CircularStrokeCap.round,
+                            radius: 180.0,
+                            lineWidth: 15.0,
+                            percent: restPercent,
+                            animation: true,
+                            progressColor: Colors.pinkAccent[400],
+                            center: CircularPercentIndicator(
+                              backgroundColor: Colors.transparent,
+                              reverse: true,
+                              circularStrokeCap: CircularStrokeCap.round,
+                              radius: 140.0,
+                              lineWidth: 15.0,
+                              percent: workPercent,
+                              animation: true,
+                              progressColor: Colors.lightBlueAccent[700],
+                              center: IconButton(
+                                onPressed: () {},
+                                icon: Icon(CupertinoIcons.playpause_fill),
+                              ),
+                            ),
+                          ),
                         ),
-                      ),
+                      ],
                     ),
                   ),
                   SizedBox(height: 30),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 5),
-                    child: Container(
-                      decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Colors.red,
-                            width: 4,
-                          ),
-                          borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(10),
-                            topRight: Radius.circular(10),
-                          )),
-                      child: Column(
-                        children: [
-                          SizedBox(height: 10),
-                          Text("Promodoro Time Settings"),
-                          SizedBox(height: 20),
-                          Row(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: [
-                              Card(
-                                child: Column(
-                                  children: [
-                                    Padding(
-                                      padding: const EdgeInsets.all(8.0),
-                                      child: Text("Work Time"),
-                                    ),
-                                    Padding(
-                                      padding: const EdgeInsets.only(
-                                          left: 8, right: 8, bottom: 8),
-                                      child: Text("$worktimeMin"),
-                                    )
-                                  ],
-                                ),
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                            color: Theme.of(context).accentColor, width: 5),
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Column(
+                      children: [
+                        Text("Promodoro Time Settings"),
+                        SizedBox(height: 10),
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            ElevatedButton(
+                              onPressed: () {},
+                              child: Column(
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Text("Work Time"),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.only(
+                                        left: 8, right: 8, bottom: 8),
+                                    child: Text("$workTimeMin Mins"),
+                                  )
+                                ],
                               ),
-                              Card(
-                                child: Column(
-                                  children: [
-                                    Text("Break Time"),
-                                    Text("$resttimeMin")
-                                  ],
-                                ),
+                            ),
+                            ElevatedButton(
+                              onPressed: () {},
+                              child: Column(
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Text("Rest Time"),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.only(
+                                        left: 8, right: 8, bottom: 8),
+                                    child: Text("$restTimeMin Mins"),
+                                  )
+                                ],
                               ),
-                              Card(
-                                child: Column(
-                                  children: [
-                                    Text("No. of Rounds"),
-                                    Text("2"),
-                                  ],
-                                ),
+                            ),
+                            ElevatedButton(
+                              onPressed: () {},
+                              child: Column(
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Text("No Of Rounds"),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.only(
+                                        left: 8, right: 8, bottom: 8),
+                                    child: Text("$rounds"),
+                                  )
+                                ],
                               ),
-                            ],
-                          ),
-                        ],
-                      ),
+                            ),
+                          ],
+                        ),
+                      ],
                     ),
                   )
                 ],
