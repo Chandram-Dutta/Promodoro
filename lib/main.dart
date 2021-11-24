f50586a7a248442505e72ebff553ca291904d75f
Chandram-Dutta <chandramdutta2004@gmail.com>
2021-11-24 20:50:52 +0530

    Provider Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:percent_indicator/percent_indicator.dart';
+import 'package:promodoro/providers.dart';
+
+final buttonIconProvider = StateProvider((_) => Icons.play_arrow);
 
 void main() {
-  runApp(>MyApp());
+  runApp(ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -41,27 +45,55 @@ class Home extends StatelessWidget {
             SizedBox(
               height: 20,
             ),
-            CircularPercentIndicator(
-              radius: 200.0,
-              lineWidth: 10.0,
-              percent: 1.0,
-              progressColor: Colors.red,
-              center: CircularPercentIndicator(
-                radius: 150.0,
-                lineWidth: 10.0,
-                percent: 1.0,
-                progressColor: Colors.green,
-                center: CircularPercentIndicator(
-                  radius: 100.0,
-                  lineWidth: 10.0,
-                  percent: 1.0,
-                  center: IconButton(
-                      onPressed: () {}, icon: Icon(Icons.play_arrow)),
-                  progressColor: Colors.blue,
-                ),
-              ),
-            )
+            Indicators()
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class Indicators extends ConsumerStatefulWidget {
+  const Indicators({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  _IndicatorsState createState() => _IndicatorsState();
+}
+
+class _IndicatorsState extends ConsumerState<Indicators> {
+  @override
+  void initState() {
+    super.initState();
+    // 3. use ref.read() in the widget life-cycle methods
+    // final value = ref.read(ProviderProj().buttonIconProvider);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CircularPercentIndicator(
+      radius: 200.0,
+      lineWidth: 10.0,
+      percent: 1.0,
+      progressColor: Colors.red,
+      center: CircularPercentIndicator(
+        radius: 150.0,
+        lineWidth: 10.0,
+        percent: 1.0,
+        progressColor: Colors.green,
+        center: CircularPercentIndicator(
+          radius: 100.0,
+          lineWidth: 10.0,
+          percent: 1.0,
+          center: IconButton(
+              onPressed: () {
+                setState(() {
+                  ref.read(buttonIconProvider.state).state = Icons.pause;
+                });
+              },
+              icon: Icon(ref.watch(buttonIconProvider))),
+          progressColor: Colors.blue,
         ),
       ),
     );
---
+++ lib/providers.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class ProviderProj {
+  final buttonIconProvider = StateProvider((_) => Icons.play_arrow);
+}
