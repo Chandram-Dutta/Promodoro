b0d998e7d3e21544bd02a02fce7f4da7d59b8e53
Chandram-Dutta <chandramdutta2004@gmail.com>
2021-11-26 17:58:48 +0530

    Added Cards

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -33,10 +33,60 @@ class Home extends StatelessWidget {
                 ),
               ),
               height: MediaQuery.of(context).size.height - 296,
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 30,
+                  ),
+                  PauseTimerSettings(),
+                ],
+              ),
             )
           ],
         ),
       ),
+    );
+  }
+}
+
+class PauseTimerSettings extends StatelessWidget {
+  const PauseTimerSettings({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Card(
+          child: SizedBox(
+            height: 75,
+            width: MediaQuery.of(context).size.width / 3 - 20,
+            child: Column(
+              children: [Text("Work Time")],
+            ),
+          ),
+        ),
+        Card(
+          child: SizedBox(
+            height: 75,
+            width: MediaQuery.of(context).size.width / 3 - 20,
+            child: Column(
+              children: [Text("Rest Time")],
+            ),
+          ),
+        ),
+        Card(
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width / 3 - 20,
+            height: 75,
+            child: Column(
+              children: [Text("Rounds")],
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
